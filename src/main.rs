6132ad9fb67a8eb9ddc8801e0b3a2619ffb78359
jerry <calljerryli@outlook.com>
2024-08-20 04:55:37 +0800

    update data

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ async fn main() {
     let page_limit = 12;
     let mut page_offset = 0;
 
-    let snapshot_count_init = 10;
-    let mut snapshot_count = snapshot_count_init;
+    let snapshot_coutdown_init = 10;
+    let mut snapshot_countdown = snapshot_coutdown_init;
 
     loop {
         let url = format!("https://www.gcores.com/gapi/v1/radios?page[limit]={}&page[offset]={}&sort=-published-at&filter[list-all]=0&fields[radios]=title%2Cdesc%2Cexcerpt%2Cis-published%2Cthumb%2Capp-cover%2Ccover%2Ccomments-count%2Clikes-count%2Cbookmarks-count%2Cis-verified%2Cpublished-at%2Coption-is-official%2Coption-is-focus-showcase%2Cduration%2Cdraft%2Caudit-draft%2Cuser%2Ccomments%2Ccategory%2Ctags%2Centries%2Centities%2Csimilarities%2Clatest-collection%2Ccollections%2Coperational-events%2Cportfolios%2Ccatalog-tags%2Cmedia%2Cdjs%2Clatest-album%2Calbums%2Cis-free&meta[categories]=%2C&meta[users]=%2C", page_limit, page_offset);
@@ -31,23 +31,23 @@ async fn main() {
                 result.extend(data);
             }
             None => {
-                tracing::info!("{:#?}", response_json);
+                tracing::warn!("response_json.data is None {:#?}", response_json);
                 break;
             }
         };
 
         page_offset += page_limit;
 
-        snapshot_count -= 1;
-        if snapshot_count <= 0 {
-            tracing::info!("{:#?}", result);
+        snapshot_countdown -= 1;
+        if snapshot_countdown <= 0 {
             write_to_csv(&result).unwrap();
-            snapshot_count = snapshot_count_init;
+            snapshot_countdown = snapshot_coutdown_init;
         }
 
-        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+        tokio::time::sleep(std::time::Duration::from_millis(500)).await;
     }
 
+    tracing::info!("sort_by...");
     result.sort_by(|a, b| {
         b.attributes
             .bookmarks_count
@@ -55,5 +55,9 @@ async fn main() {
             .unwrap_or(0)
             .cmp(&a.attributes.bookmarks_count.as_i64().unwrap_or(0))
     });
+
+    tracing::info!("write_to_csv...");
     write_to_csv(&result).unwrap();
+
+    tracing::info!("finished");
 }
