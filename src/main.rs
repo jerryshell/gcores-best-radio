66a901821cbd3561a3a69fc1b0c564dc52f153a5
Jerry <calljerryli@outlook.com>
2025-08-12 03:46:30 +0800

    lean model

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ async fn main() {
 
     loop {
         let url = format!(
-            "https://www.gcores.com/gapi/v1/radios?page[limit]={page_limit}&page[offset]={page_offset}&sort=-published-at&filter[list-all]=0&fields[radios]=title%2Cdesc%2Cexcerpt%2Cis-published%2Cthumb%2Capp-cover%2Ccover%2Ccomments-count%2Clikes-count%2Cbookmarks-count%2Cis-verified%2Cpublished-at%2Coption-is-official%2Coption-is-focus-showcase%2Cduration%2Cdraft%2Caudit-draft%2Cuser%2Ccomments%2Ccategory%2Ctags%2Centries%2Centities%2Csimilarities%2Clatest-collection%2Ccollections%2Coperational-events%2Cportfolios%2Ccatalog-tags%2Cmedia%2Cdjs%2Clatest-album%2Calbums%2Cis-free&meta[categories]=%2C&meta[users]=%2C"
+            "https://www.gcores.com/gapi/v1/radios?page[limit]={page_limit}&page[offset]={page_offset}&sort=-published-at&fields[radios]=title%2Cdesc%2Cexcerpt%2Cthumb%2Ccover%2Ccomments-count%2Clikes-count%2Cbookmarks-count%2Cpublished-at%2Cduration"
         );
         tracing::info!("{}", url);
 
